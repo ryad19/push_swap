3206d99c1acf1c1e0089a5f9932d6774a86627cf
bhdsara <111253033+bhdsara@users.noreply.github.com>
2026-06-09 13:29:47 +0200

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -40,6 +40,8 @@ void	free_matrix(char **matrix);
 void	sa(t_stack **stack_a);
 void	sb(t_stack **stack_b);
 void	ss(t_stack **stack_a, t_stack **stack_b);
+void	pa(t_stack **stack_a, t_stack **stack_b);
+void	pb(t_stack **stack_b, t_stack **stack_a);
 
 
 #endif
