ed4bd4b0a03bf696626cfcdfe101f6c143ec7283
bhdsara <111253033+bhdsara@users.noreply.github.com>
2026-06-09 14:02:54 +0200

    Update push_swap.h
    adding some function

--- push_swap.h
+++ push_swap.h
@@ -45,6 +45,10 @@ void	pb(t_stack **stack_b, t_stack **stack_a);
 void	ra(t_stack **stack_a);
 void	rb(t_stack **stack_b);
 void	rr(t_stack **stack_a, t_stack **stack_b);
+void	rra(t_stack **stack_a);
+void	rrb(t_stack **stack_b);
+void	rrr(t_stack **stack_a, t_stack **stack_b);
+
 
 
 #endif
